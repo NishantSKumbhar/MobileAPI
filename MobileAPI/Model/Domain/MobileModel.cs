9f7c94a1469ddb52844d5cf03b730ed4351f29d9
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-03 09:45:07 +0530

    Model Developed

--- MobileAPI/Model/Domain/MobileModel.cs
+++ MobileAPI/Model/Domain/MobileModel.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Color { get; set; }
         public float Price { get; set; }
         public float Discount { get; set; }
         public string? MobileImageUrl { get; set; }
