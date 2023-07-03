e99730247a13a998d78637d458107f433bdff3c7
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-03 10:22:13 +0530

    Initialize the DbContext Class.

---
+++ MobileAPI/Data/MobileDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileAPI.Model.Domain;
+
+namespace MobileAPI.Data
+{
+    public class MobileDbContext: DbContext
+    {
+        public MobileDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        // When we run entity core migrations, Below property will create tables in database.
+        public DbSet<MobileModel> MobileT { get; set; }
+    }
+}
