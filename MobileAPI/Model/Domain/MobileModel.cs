50e50a41594d7951b8281836d3874054aedbcf71
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-03 09:43:18 +0530

    Add project files.

---
+++ MobileAPI/Model/Domain/MobileModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileAPI.Model.Domain
+{
+    public class MobileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public float Discount { get; set; }
+        public string? MobileImageUrl { get; set; }
+    }
+}
